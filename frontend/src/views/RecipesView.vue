5cea4b51b244c114cd5344dc1a6b2042f00252a2
py6jlb <py6jlb@gmail.com>
2026-01-25 12:57:15 +0000

    -начал делать страницу рецептов

--- frontend/src/views/RecipesView.vue
+++ frontend/src/views/RecipesView.vue
@@ -1,22 +1,20 @@
-<script setup>
-import { onMounted } from 'vue'
-import { storeToRefs } from 'pinia'
-import { useRecipesStore } from '../stores/recipes.store'
-
-const recipesStore = useRecipesStore()
-const { recipes, loading, error } = storeToRefs(recipesStore)
-
-onMounted(() => {
-  recipesStore.fetchRecipes()
-})
-</script>
-
 <template>
-  <div >
-    <div >
-      <div v-if="loading" >
-        <span ></span>
-        <span >Загрузка рецептов...</span>
+  <div>
+    <v-toolbar border floating class="w-100" rounded="lg">
+      <div class=" d-flex w-100 px-2 py-1 align-center">
+        <v-text-field density="comfortable" placeholder="Поиск" variant="solo" clearable flat hide-details single-line />
+        <v-btn size="large" class="mx-2">Искать</v-btn>
+      </div>
+
+      <template v-slot:prepend>
+        <v-btn icon="mdi-plus"></v-btn>
+        <v-divider class="mx-1" vertical></v-divider>
+      </template>
+    </v-toolbar>
+    <div>
+      <div v-if="loading">
+        <span></span>
+        <span>Загрузка рецептов...</span>
       </div>
       <div v-else-if="error">
         {{ error }}
@@ -29,3 +27,16 @@ onMounted(() => {
     </div>
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useRecipesStore } from '../stores/recipes.store'
+
+const recipesStore = useRecipesStore()
+const { recipes, loading, error } = storeToRefs(recipesStore)
+
+onMounted(() => {
+  recipesStore.fetchRecipes()
+})
+</script>
