af5f15a2ea75a304a78eefea0dd87d0770f0b37b
py6jlb <py6jlb@gmail.com>
2026-03-09 09:39:44 +0000

    -переделал отображение списка рецептов

--- frontend/src/views/RecipesView.vue
+++ frontend/src/views/RecipesView.vue
@@ -76,38 +76,51 @@
           Рецепты не найдены
         </div>
         <div v-else>
-          <v-row>
-            <v-col v-for="recipe in recipes.items" :key="recipe.id" cols="12" sm="6" md="4">
-              <v-card
-                variant="outlined"
-                class="h-100 cursor-pointer"
-                @click="goToRecipe(recipe.id)"
-              >
+          <v-card
+            v-for="recipe in recipes.items"
+            :key="recipe.id"
+            variant="outlined"
+            class="mb-3 cursor-pointer recipe-card"
+            @click="goToRecipe(recipe.id)"
+          >
+            <div class="d-flex">
+              <!-- Превью картинки 128x128 -->
+              <div v-if="recipe.files?.length > 0" class="recipe-image-container flex-shrink-0">
+                <!-- Размытый фон -->
                 <v-img
-                  v-if="recipe.files?.length > 0"
                   :src="getFileUrl(recipe.files[0].id)"
-                  height="150"
+                  width="128"
+                  height="128"
                   cover
-                  class="bg-grey-lighten-2"
+                  class="recipe-blur-bg"
                 ></v-img>
+                <!-- Основное изображение по центру -->
                 <v-img
-                  v-else
-                  height="150"
-                  cover
-                  class="bg-grey-lighten-2 d-flex align-center justify-center"
-                >
-                  <v-icon size="48" color="grey">mdi-food</v-icon>
-                </v-img>
-                <v-card-title>{{ recipe.title }}</v-card-title>
-                <v-card-text v-if="recipe.descriptions">
-                  {{ recipe.descriptions }}
-                </v-card-text>
-                <v-card-text v-if="recipe.tags?.length">
+                  :src="getFileUrl(recipe.files[0].id)"
+                  width="128"
+                  height="128"
+                  contain
+                  class="recipe-main-img"
+                ></v-img>
+              </div>
+              <div
+                v-else
+                class="flex-shrink-0 bg-grey-lighten-2 d-flex align-center justify-center"
+                style="width: 128px; height: 128px"
+              >
+                <v-icon size="48" color="grey">mdi-food</v-icon>
+              </div>
+
+              <!-- Контент -->
+              <div class="pa-3 flex-grow-1 d-flex flex-column">
+                <div class="text-subtitle-1 font-weight-medium mb-1">{{ recipe.title }}</div>
+
+                <div v-if="recipe.tags?.length" class="mb-1">
                   <v-chip
                     v-for="tag in recipe.tags"
                     :key="tag.id"
                     size="x-small"
-                    class="mr-1 mb-1"
+                    class="mr-1"
                     :style="{
                       backgroundColor: tag.color || '#757575',
                       color: getContrastColor(tag.color),
@@ -115,10 +128,14 @@
                   >
                     {{ tag.name }}
                   </v-chip>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
+                </div>
+
+                <div v-if="recipe.descriptions" class="text-body-2 text-grey-darken-1 recipe-description">
+                  {{ truncateDescription(recipe.descriptions) }}
+                </div>
+              </div>
+            </div>
+          </v-card>
 
           <!-- Пагинация -->
           <div v-if="recipes?.totalCount > recipes?.pageSize" class="d-flex justify-center mt-4">
@@ -150,6 +167,10 @@ import { useRecipesStore } from '../stores/recipes.store'
 import { useTagsStore } from '../stores/tags.store'
 import FilterDialog from '../components/FilterDialog.vue'
 import { getContrastColor } from '../utils/colors'
+import { marked } from 'marked'
+
+// Настройка marked
+marked.setOptions({ breaks: true, gfm: true })
 
 const router = useRouter()
 const recipesStore = useRecipesStore()
@@ -214,6 +235,20 @@ const getFileUrl = (fileId) => {
   return `${import.meta.env.VITE_API_BASE_URL}/files/${fileId}`
 }
 
+// Обрезание и рендеринг markdown описания
+const truncateDescription = (text) => {
+  if (!text) return ''
+  // Рендерим markdown в HTML
+  const html = marked.parse(text)
+  // Убираем HTML теги
+  const plainText = html.replace(/<[^>]*>/g, '')
+  // Обрезаем до 180 символов
+  if (plainText.length > 180) {
+    return plainText.substring(0, 180) + '...'
+  }
+  return plainText
+}
+
 const goToRecipe = (id) => {
   router.push(`/recipe/${id}`)
 }
@@ -226,5 +261,39 @@ onMounted(() => {
 <style scoped>
 .cursor-pointer {
   cursor: pointer;
+}
+
+.recipe-card:hover {
+  background-color: rgb(var(--v-theme-surface));
+  border-color: rgb(var(--v-theme-primary));
+}
+
+.recipe-description {
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+
+.recipe-image-container {
+  position: relative;
+  width: 128px;
+  height: 128px;
+  overflow: hidden;
+  flex-shrink: 0;
+}
+
+.recipe-blur-bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  filter: blur(20px);
+  transform: scale(1.2);
+  opacity: 0.5;
+}
+
+.recipe-main-img {
+  position: relative;
+  z-index: 1;
 }
 </style>
